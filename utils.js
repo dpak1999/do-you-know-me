4d8f8ad80bf7f48d297f5765630208c7f175ac27
dpak1999 <deepakkumardash313@gmail.com>
2021-08-18 19:36:14 +0530

    add utils functions

---
+++ utils.js
@@ -0,0 +1,35 @@
+/** @format */
+
+export const questionsArray = [
+  {
+    question: "What is my birth Month?",
+    answer: "november",
+  },
+  {
+    question: "To which state do I belong?",
+    answer: "odisha",
+  },
+  {
+    question: "What is the name of the school where i studied?",
+    answer: "kendriya vidyalaya",
+  },
+  {
+    question: "What is my Favorite food?",
+    answer: "biryani",
+  },
+  {
+    question: "What is my favourite pet?",
+    answer: "dog",
+  },
+];
+
+export const highScore = [
+  {
+    name: "Arijit",
+    score: 5,
+  },
+  {
+    name: "Raj",
+    score: 4,
+  },
+];
